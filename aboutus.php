4f39de52f439e2f4684c64d3d814080cf2f5cc37
ARYAN SRIVASTAVA <srivastavaaryan23@gmail.com>
2023-06-15 19:20:48 +0530

    new

---
+++ aboutus.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <style type="text/css">
+            h5{
+                color: blue;
+            }
+            .p1{
+                text-align: justify;
+            }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About us | Life Style Store</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class="container" id="content">
+    <img src="#" style="float: left;">
+    <div class="row">
+        <div class="col-lg-4" align="justify">
+            <h3><mark>WHO WE ARE</mark></h3><br>
+            <p id="p1">Musical store is a recognized leader in the ecommerce industry, with a steadfast commitment to success and
+                 a record of achievement that continues a tradition of delivering excellence.<br>
+                 <br>Musical Store is guided by four principles: customer obsession rather than competitor focus,
+                  passion for invention, commitment to operational excellence, and long-term thinking.<br>
+                  <br><h4><mark>Vision</mark></h4>Bringing convenience to all the musicians out there by picking the best quality instruments for them.<br>
+                  <br><h4><mark>Mission</mark></h4>We strive to offer our customers the lowest possible prices, the best available selection,
+                   and the utmost convenience.
+            </p>
+        </div>
+    </div>
+  </div>
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
